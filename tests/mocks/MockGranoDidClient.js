89e0e5bafac54a6bf7df0eac2df1b8b8731a2926
intellltech <danieljames.maxdev@gmail.com>
2022-11-23 14:21:26 +0900

    rename tests

---
+++ tests/mocks/MockGranoDidClient.js
@@ -0,0 +1,17 @@
+// @ts-check
+'use strict'
+
+/**
+ * GranoDidClient.
+ */
+class MockGranoDidClient {
+  /**
+   * controller.
+   *
+   */
+  async controller () {
+    return
+  }
+}
+
+module.exports = MockGranoDidClient
--- tests/mocks/MockGranoDidConfig.js
+++ tests/mocks/MockGranoDidConfig.js
@@ -1,18 +1,18 @@
 // @ts-check
 'use strict'
 
-const { DidConfig } = require('@eg-easy/grano-did-client')
+const { GranoDidConfig } = require('@eg-easy/grano-did-client')
 
-const mockDidConfigParams = {
+const mockGranoDidConfigParams = {
   endPoint: 'http://localhost:26657',
   mnemonic: 'estate giraffe icon february goat observe actor left armed zone million note system myth coconut series calm steak dinosaur twin immune mansion morning drastic',
   prefix: 'wasm',
   fromAddress: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
 }
 
-const mockDidConfig = DidConfig.create(mockDidConfigParams)
+const mockGranoDidConfig = GranoDidConfig.create(mockGranoDidConfigParams)
 
 module.exports = {
-  mockDidConfigParams,
-  mockDidConfig,
+  mockGranoDidConfigParams,
+  mockGranoDidConfig,
 }
